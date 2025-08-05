b32ce077d55dbb5d49006718206d04c0b3bdc314
Golokova Sofya <113624984+khobaess@users.noreply.github.com>
2025-08-05 10:09:00 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'https://resume-dev.onrender.com',
+        target: {https},
         changeOrigin: true,
         secure: false,
       },
